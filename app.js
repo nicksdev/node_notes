18898163a6fa6a819a15bc364b87e540cdfde980
Nick Hughes <nick@nickhughes.io>
2020-12-13 21:45:09 +1100

    Create and import custom module

---
+++ app.js
@@ -0,0 +1,12 @@
+
+const getNotes = require('./notes.js')
+
+
+console.log(getNotes())
+
+
+
+// 1. Create a new file called notes.js
+// 2. Create a getNotes function that returns "Your notes...."
+// 3. Export getNotes function
+// 4. Form app.js. load in and call the function, printing message to console
