5cd50089612468685e181103117df7df05a5b365
Nick Hughes <nick@nickhughes.io>
2020-12-14 22:09:48 +1100

    Setup yargs with initial commands

--- app.js
+++ app.js
@@ -1,15 +1,59 @@
 
+const yargs = require('yargs')
+
 const getNotes = require('./notes.js')
 
-// assign command line input to variable 'command'
-const command = process.argv[2]
+
+// Create add command
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
+    }
+})
+
+// Create remove command
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing a note...')
+    }
+})
+
+// Create list command
+
+yargs.command({
+    command: 'list',
+    describe: 'List notes',
+    handler: function () {
+        console.log('Listing notes...')
+    }
+})
 
 
-// Perform actions based on command line input
-if (command === 'add') {
-    console.log('Adding note!')
-} else if (command === 'remove') {
-    console.log('Removing note!')
-}
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note...')
+    }
+})
 
-
+yargs.parse()
